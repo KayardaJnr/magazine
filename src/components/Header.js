119bdfda46e688d84d647ba454bd0cbcfe72ca14
Sulaiman Sani <sulaimansani152@gmail.com>
2025-10-22 12:28:31 +0100

    version 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ const Header = () => {
       <h1> Ihifix Magazine</h1>
       <nav>
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#articles">Articles</a></li>
-          <li><a href="#news">News</a></li>
-          <li><a href="#about">About</a></li>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/articles">Articles</a></li>
+          <li><a href="/news">News</a></li>
+          <li><a href="/about">About</a></li>
         </ul>
       </nav>
     </header>
